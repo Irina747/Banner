4b10c1e193cc23f9955e7eae37c1c9cff829b7b7
Irina747 <65604935+Irina747@users.noreply.github.com>
2020-12-04 18:15:27 +0300

    new commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Label from "./Components/Label";
 import Header from "./Components/Header";
 import Image from "./Components/Image";
 import Input from "./Components/Input";
+import {exportComponentAsPNG} from "react-component-export-image";
 
 class App extends Component {
     state = {
@@ -25,6 +26,11 @@ class App extends Component {
         file: '',
         imagePreviewUrl: ''
 
+    }
+
+    constructor(props) {
+        super(props);
+        this.componentRef = React.createRef();
     }
 
     _handleImageChange(e) {
@@ -94,14 +100,52 @@ class App extends Component {
         })
     }
 
+    handleJSON = () => {
+        let previewItem = document.querySelector(".preview")
+        let imageItem = document.querySelector(".image")
+        let contextItem = document.querySelector(".context")
+        let imgItem = document.querySelector("img")
+
+        let jsonObject = {
+            "preview": {
+                "type": "div",
+                "className": previewItem.className,
+                "onClick": previewItem.onClick,
+                "ref": previewItem.ref,
+                "image": {
+                    "type": "div",
+                    "className": imageItem.className,
+                    "img": {"type": "img", "src": imgItem.src, "alt": imgItem.alt}
+                },
+                "context": {
+                    "type": "p",
+                    "className": contextItem.className,
+                    "content": this.state.content
+                },
+                "link": this.state.link
+            }
+        }
+
+        let json = JSON.stringify(jsonObject);
+
+        document.addEventListener('copy', (event) => {
+            event.clipboardData.setData('text/plain', json);
+            event.preventDefault();
+        });
+
+        alert("JSON скопирован в буфер обмена");
+    }
+
+    handleJSX = () => {
+
+    }
 
     render() {
         let {imagePreviewUrl} = this.state;
         let $imagePreview = <Image/>;
         if (imagePreviewUrl) {
-            $imagePreview = (<img src={imagePreviewUrl} alt={"banner"} />);
+            $imagePreview = (<img src={imagePreviewUrl} alt={"banner"}/>);
         }
-
 
         return (
             <div className="App">
@@ -110,44 +154,49 @@ class App extends Component {
                     <Header text={'Настройки баннера'}/>
 
                     <Label text={'Загрузите изображение'}/>
-                    <Loading
-                        onChange={(e)=>this._handleImageChange(e)}
-                    />
+                    <Loading onChange={(e) => this._handleImageChange(e)}/>
 
                     <Label text={'Укажите текст баннера'}/>
-                    <Input placeholder={this.state.content}
-                           onChange={this.handleInput}/>
+                    <Input placeholder={this.state.content} onChange={this.handleInput}/>
 
                     <Label text={'Заливка градиентом'}/>
 
-                    <button className='color orange' onClick={this.onChangeOrange}> </button>
-                    <button className='color blue' onClick={this.onChangeBlue}> </button>
-                    <button className='color green' onClick={this.onChangeGreen}> </button>
-                    <button className='color pink' onClick={this.onChangePink}> </button>
-                    <button className='color brown' onClick={this.onChangeBrown}> </button>
-                    <button className='color purple' onClick={this.onChangePurple}> </button>
+                    <button className='color orange' onClick={this.onChangeOrange}></button>
+                    <button className='color blue' onClick={this.onChangeBlue}></button>
+                    <button className='color green' onClick={this.onChangeGreen}></button>
+                    <button className='color pink' onClick={this.onChangePink}></button>
+                    <button className='color brown' onClick={this.onChangeBrown}></button>
+                    <button className='color purple' onClick={this.onChangePurple}></button>
 
                     <Label text={'Ссылка'}/>
-                    <Input placeholder={this.state.link}
-                           onChange={this.handleLink}
-                    />
+                    <Input placeholder={this.state.link} onChange={this.handleLink}/>
                 </div>
 
                 <div className="column">
                     <Header text={'Превью баннера'}/>
 
-                    <section className={this.state.backgroundClass} onClick={this.newDoc}>
+                    <div className={this.state.backgroundClass}
+                         onClick={this.newDoc}
+                         ref={this.componentRef}>
                         <div className="image">
                             {$imagePreview}
                         </div>
                         <p className="context">{this.state.content}</p>
-                    </section>
+                    </div>
 
                     <Export/>
 
-                    <button className="export">PNG</button>
-                    <button className="export">JSX</button>
-                    <button className="export">JSON</button>
+                    <React.Fragment>
+                        <button className="export"
+                                onClick={() => exportComponentAsPNG(this.componentRef)}
+                        >PNG</button>
+                        <button className="export"
+                                onClick={this.handleJSON}
+                        >JSON</button>
+                        <button className="export"
+                                onClick={this.handleJSX}
+                        >JSX</button>
+                    </React.Fragment>
 
                 </div>
             </div>
